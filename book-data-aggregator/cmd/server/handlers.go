46d2fdada532294b56a9f4d8bcc8133251efbed9
Najmus Sakib Dhrubo <najmussakibdhrubo52@gmail.com>
2025-10-16 02:06:02 +0600

    Fetched data using url.

--- book-data-aggregator/cmd/server/handlers.go
+++ book-data-aggregator/cmd/server/handlers.go
@@ -3,7 +3,48 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io"
+	"encoding/json"
+	"strconv"
 )
+
+// We need an empty interface type because the json response
+// can have multiple types of values
+type jsonData interface{}
+
+type result struct {
+	TotalBooks int
+	EarliestPublicationYear int
+	LatestPublicationYear int
+	Languages []string
+	Authors []string
+}
+
+func (res *result) dataRequest(title string, pageNumber int) {
+	response, err := http.Get(fmt.Sprintf("https://openlibrary.org/search.json?title=%s&limit=100&page=%d", title, pageNumber))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer response.Body.Close()
+
+	byteData, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var j jsonData
+
+	err = json.Unmarshal(byteData, &j)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("%+v", j)
+}
 
 
 func getData(w http.ResponseWriter, r *http.Request) {
@@ -15,6 +56,15 @@ func getData(w http.ResponseWriter, r *http.Request) {
 	queryParams := r.URL.Query()
 
 	title := queryParams.Get("title")
+	page, err := strconv.Atoi(queryParams.Get("page"))
 
-	fmt.Println(title)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var res *result
+
+	res.dataRequest(title, page)	
+	
 }
