51c59c8c29ef0d1cd95e1b0ed0f4d544cba13761
Najmus Sakib Dhrubo <najmussakibdhrubo52@gmail.com>
2025-10-19 20:43:57 +0600

    Small change in Print function.

--- book-data-aggregator/cmd/server/main.go
+++ book-data-aggregator/cmd/server/main.go
@@ -14,8 +14,8 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/get-data", getData)
 
-	fmt.Printf("Starting Server at 127.0.0.1:8000\n")
-	fmt.Printf("Visit: 127.0.0.1:8000\n")
+	fmt.Println("Starting Server at 127.0.0.1:8000")
+	fmt.Println("Visit: 127.0.0.1:8000")
 
 	err := http.ListenAndServe("127.0.0.1:8000", mux)
 	if err!=nil {
